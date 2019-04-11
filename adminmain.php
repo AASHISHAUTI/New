901a183e78b07ce4f44708020d5ac42be7cecac4
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-11 18:47:21 +0530

    Update adminmain.php

--- adminmain.php
+++ adminmain.php
@@ -1,16 +1,18 @@
 <html>
 <head>
+	<link rel="stylesheet" href="Des.css">
 
 	<title>Admin Main Page</title>
 	</head>
 	<center><br><br>
 
 <button onclick="location.href = 'adduser.php';" id="myButton" class="float-left submit-button" >Add Student</button><br><br><br>
+<!--<button onclick="location.href = 'DeleteUser.php';" id="myButton" class="float-left submit-button" >Remove Student</button><br><br><br>-->
 <button onclick="location.href = 'showuserdata.php';" id="myButton" class="float-left submit-button" >Show Student Data</button>
 </center>
 	<body bgcolor="cyan">
 		<?php
-		echo '<a href="Choose.php"><h2 align = "center">Logout</h2></a>';
+		echo '<a href="choose.php"><h2 align = "center">Logout</h2></a>';
 		?>
 	</body>
 </html>

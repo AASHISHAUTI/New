2f803fa07e3707dc421e309e5933cd871447210c
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-11 18:49:38 +0530

    Update showuserdata.php

--- showuserdata.php
+++ showuserdata.php
@@ -1,8 +1,8 @@
 
 
 
-		
-	
+
+
 
 <!DOCTYPE html>
 <html>
@@ -14,7 +14,7 @@
                 border: 1px solid black;
             }
         </style>
-  
+
 
 
 
@@ -29,7 +29,7 @@ if(isset($_POST['search']))
     // using concat mysql function
     $query = "SELECT * FROM `studup` WHERE CONCAT(`name`, `dt`, `ctime`,`Message`) LIKE '%".$valueToSearch."%'";
     $search_result = filterTable($query);
-    
+
 }
  else {
     $query = "SELECT * FROM `studup`";
@@ -39,7 +39,7 @@ if(isset($_POST['search']))
 // function to connect and execute the query
 function filterTable($query)
 {
-    $connect = mysqli_connect("localhost", "root", "root", "Daily_Report_Interns");
+    $connect = mysqli_connect("localhost", "root", "", "Daily_Report_Interns");
     $filter_Result = mysqli_query($connect, $query);
     return $filter_Result;
 }
@@ -48,6 +48,9 @@ function filterTable($query)
 
 
 
+
+
+	<link rel="stylesheet" href="Des.css"
 
 
 
@@ -61,36 +64,35 @@ function filterTable($query)
 
 
 
-
-
     <body><center>
-        
+
         <form action="showuserdata.php" method="post">
             <input type="text" name="valueToSearch" placeholder="Value To Search"><br><br>
             <input type="submit" name="search" value="Filter"><br><br>
-            
-            <table>
+
+            <table class="blueTable">
                 <tr>
-                   
+
                     <th>Name</th>
                      <th>Date</th>
-                     <th>CTime</th>
+                     <th>Time</th>
                      <th>Message</th>
                 </tr>
 
-      <!-- populate table from mysql database -->
+      
                 <?php while($row = mysqli_fetch_array($search_result)):?>
                 <tr>
                     <td><?php echo $row['name'];?></td>
                     <td><?php echo $row['dt'];?></td>
                     <td><?php echo $row['CTime'];?></td>
                     <td><?php echo $row['message'];?></td>
-              
+
                 </tr>
                 <?php endwhile;?>
             </table>
         </form>
         </center>
+
     </body>
     <?php
 echo '<a href="adminmain.php"><h2 align = "center">Back</h2></a>';

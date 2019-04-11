7ec2b2836d34277d8bc8f75dac3663db9d4a0f5b
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-11 19:37:47 +0530

    Update studup.php

--- studup.php
+++ studup.php
@@ -32,7 +32,7 @@ select{width: 15.5em}
 	<select name="Name">
 		<?php
 		include("config.inc.php");
- $conn = mysqli_connect($server, $username, $password,'Daily_Intern_Reports');
+ $conn = mysqli_connect($server, $username, $password,'Daily_Report_Interns');
 		if(!$conn)
 		{
 			die('Connection failed!'.mysqli_error($conn));
@@ -59,7 +59,7 @@ select{width: 15.5em}
 <label>Date:</label></td><td>
  <input type="text" id="datepicker" name="dt">
  <td>
-
+<label>Time:-</label>
 <input type="text" value="<?php
 date_default_timezone_set('Asia/Calcutta');
 $current_time=date('h:i:s:a');

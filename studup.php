2ad2ed17591eba31a9ecccbfaf821ef5c33bf0d8
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-11 19:28:46 +0530

    Update studup.php

--- studup.php
+++ studup.php
@@ -31,7 +31,8 @@ select{width: 15.5em}
 <td>
 	<select name="Name">
 		<?php
-		$conn = mysqli_connect('127.0.0.1','root','','Daily_Report_Interns');
+		include("config.inc.php");
+ $conn = mysqli_connect($server, $username, $password,'Daily_Intern_Reports');
 		if(!$conn)
 		{
 			die('Connection failed!'.mysqli_error($conn));

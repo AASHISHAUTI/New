4947fa376db264326d55ba4b84c31ccfb10ac93f
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-13 13:11:37 +0530

    Update adduser.php

--- adduser.php
+++ adduser.php
@@ -1,46 +1,52 @@
 <?php
 session_start();
-$_SESSION['user'];
-?>
-<html>
-<head>
-	<link rel="stylesheet" href="Des.css">
-	<title>Add User Form</title>
-</head>
-<body bgcolor="cyan">
-<center>
-	<h2>New User Registration</h2></center>
+if(isset($_SESSION['user'])){
+	echo '<html>
+	<head>
+		<link rel="stylesheet" href="Des.css">
+		<title>Add User Form</title>
+	</head>
+	<body bgcolor="cyan">
 	<center>
-<form action="userregdata.php" method="POST">
-<table>
-<tr>
-<td>
-<label>Name:-</label>
-</td>
-<td>
-<input type="text" name="name">
-</td>
-</tr>
+		<h2>New User Registration</h2></center>
+		<center>
+	<form action="userregdata.php" method="POST">
+	<table>
+	<tr>
+	<td>
+	<label>Name:-</label>
+	</td>
+	<td>
+	<input type="text" name="name">
+	</td>
+	</tr>
 
-<tr>
-<td>
-<label>Date of Joining:-</label>
-</td>
-<td>
-<input type="date" name="dt">
-</td>
-</tr>
-<tr>
-<td>
-<label>Address:-</label>
-</td>
-<td>
-<input type="text" name="address">
-</td>
-</tr>
-<tr><td><button type="submit" name="submit" value="Submit">Submit</button></td>
-<td><button type="reset" name="reset" value="Reset">Reset</button></td>
-</tr></table></form>
+	<tr>
+	<td>
+	<label>Date of Joining:-</label>
+	</td>
+	<td>
+	<input type="date" name="dt">
+	</td>
+	</tr>
+	<tr>
+	<td>
+	<label>Address:-</label>
+	</td>
+	<td>
+	<input type="text" name="address">
+	</td>
+	</tr>
+	<tr><td><button type="submit" name="submit" value="Submit">Submit</button></td>
+
+	</tr></table></form>';
+
+}
+else {
+	echo"<script>location.href='adminlogin.php'</script>";
+}
+?>
+
 <?php
 		echo '<a href="adminmain.php"><h2 align = "center">Back</h2></a>';
 		?>

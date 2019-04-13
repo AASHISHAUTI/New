d57e1914fd6e926012dab3f4947abdb368bf3228
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-13 13:14:37 +0530

    Update studupdate.php

--- studupdate.php
+++ studupdate.php
@@ -1,11 +1,13 @@
 <?php
 echo '<link rel="stylesheet" href="Des.css">';
+include("config.inc.php");
+ $conn = mysqli_connect($server, $username, $password);
 
-$conn = mysqli_connect($server, $username, $password);
 if(!$conn)
 {
 	die('Connection failed!'.mysqli_error($conn));
 }
+ $db = mysqli_select_db($conn, "Daily_Report_Interns");
 $name = $_POST['Name'];
 $date = $_POST['dt'];
 $time= $_POST['CTime'];
@@ -16,6 +18,9 @@ $mysql = "INSERT INTO studup(name,dt,ctime,message) VALUES('$name','$date','$tim
 if(mysqli_query($conn,$mysql))
 {
 	echo "Successfully Stored";
+	echo "<script>var timer = setTimeout(function() {
+            location.href='studup.php'
+        }, 3000);</script>";
 }
 else
 {

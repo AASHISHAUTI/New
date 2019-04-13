e93de94f2b77c704a425067b3a5f015c19f38df8
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-13 13:12:55 +0530

    Update adminmain.php

--- adminmain.php
+++ adminmain.php
@@ -4,15 +4,25 @@
 
 	<title>Admin Main Page</title>
 	</head>
-	<center><br><br>
 
-<button onclick="location.href = 'adduser.php';" id="myButton" class="float-left submit-button" >Add Student</button><br><br><br>
-<!--<button onclick="location.href = 'DeleteUser.php';" id="myButton" class="float-left submit-button" >Remove Student</button><br><br><br>-->
-<button onclick="location.href = 'showuserdata.php';" id="myButton" class="float-left submit-button" >Show Student Data</button>
+	<center><br><br>
+<?php
+session_start();
+if(isset($_SESSION['user'])){
+	echo '<a href="adduser.php"><button>Add Student</button></a><br><br><br>';
+	echo '<!--<a href="DeleteUser.php"><button >Remove Student</button></a><br><br><br>-->';
+	echo '<a href="showuserdata.php"><button>Show Student Data</button>';
+}
+else {
+	echo"<script>location.href='adminlogin.php'</script>";
+}
+?>
+
+
 </center>
 	<body bgcolor="cyan">
 		<?php
-		echo '<a href="choose.php"><h2 align = "center">Logout</h2></a>';
+		echo '<a href="logout.php"><h2 align = "center">Logout</h2></a>';
 		?>
 	</body>
 </html>

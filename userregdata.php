b4cf937474c472f3046da8509e5e1d1913b6f076
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-04-13 13:15:21 +0530

    Update userregdata.php

--- userregdata.php
+++ userregdata.php
@@ -1,24 +1,33 @@
 <?php
-echo '<link rel="stylesheet" href="Des.css">';
-include("config.inc.php");
- $conn = mysqli_connect($server, $username, $password,'Daily_Report_Interns');
-if(!$conn)
-{
-	die('Connection failed!'.mysqli_error($conn));
-}
-$name = $_POST['name'];
-$date = $_POST['dt'];
-$address = $_POST['address'];
 
-$mysql = "INSERT INTO userregdata(name,dt,address) VALUES('$name','$date','$address')";
+session_start();
+if(isset($_SESSION['user'])){
+	echo '<link rel="stylesheet" href="Des.css">';
+	include("config.inc.php");
+	$conn = mysqli_connect($server, $username, $password);
+	if(!$conn)
+	{
+		die('Connection failed!'.mysqli_error($conn));
+	}
+	$db = mysqli_select_db($conn, "Daily_Report_Interns");
+	$name = $_POST['name'];
+	$date = $_POST['dt'];
+	$address = $_POST['address'];
 
-if(mysqli_query($conn,$mysql))
-{
-	echo "Registerd Successfully";
+	$mysql = "INSERT INTO userregdata(name,dt,address) VALUES('$name','$date','$address')";
+
+	if(mysqli_query($conn,$mysql))
+	{
+		echo "Registerd Successfully";
+	}
+	else
+	{
+		echo mysqli_error($conn);
+	}
+	echo '<a href="adminmain.php"><h2 align = "center">Back</h2></a>';
 }
-else
-{
-	echo mysqli_error($conn);
+else {
+	echo"<script>location.href='adminlogin.php'</script>";
 }
-echo '<a href="adminmain.php"><h2 align = "center">Back</h2></a>';
+
 ?>
